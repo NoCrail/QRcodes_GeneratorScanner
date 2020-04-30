0518bce04222b53430bf780d110398caeece6744
NoCrail <vlad-s-d@ya.ru>
2020-04-30 17:48:58 +0300

    Сделал отправку на печать

--- app/src/main/java/com/example/barcodescannertry/MainActivity.kt
+++ app/src/main/java/com/example/barcodescannertry/MainActivity.kt
@@ -2,32 +2,34 @@ package com.example.barcodescannertry
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.media.MediaScannerConnection
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
+import android.os.StrictMode
 import android.widget.Toast
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
-import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     var bitmap : Bitmap? = null
+    val fout: FileOutputStream? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val builder: StrictMode.VmPolicy.Builder = StrictMode.VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
 
 
         btnScan.setOnClickListener {
@@ -36,6 +38,37 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        send_btn.setOnClickListener {
+            try{
+                val cachePath = File(baseContext.getCacheDir(), "images")
+                cachePath.mkdirs() // don't forget to make the directory
+
+                val stream =
+                    FileOutputStream("$cachePath/image.png") // overwrites this image every time
+
+                bitmap!!.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                stream.close()
+
+
+                val imagePath = File(baseContext.getCacheDir(), "images")
+                val newFile = File(imagePath, "image.png")
+                val contentUri: Uri? =
+                    FileProvider.getUriForFile(baseContext, "com.example.barcodescannertry.fileprovider", newFile)
+
+                if (contentUri != null) {
+                    val shareIntent = Intent()
+                    shareIntent.action = Intent.ACTION_SEND
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) // temp permission for receiving app to read this file
+                    shareIntent.setDataAndType(contentUri, contentResolver.getType(contentUri))
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
+                    startActivity(Intent.createChooser(shareIntent, "Choose an app"))
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
 
         generate_btn.setOnClickListener {
             if(qr_et.text.isEmpty()){
@@ -45,14 +78,16 @@ class MainActivity : AppCompatActivity() {
                 try {
                     bitmap = TextToImageEncode(qr_et.text.toString())
                     qr_iv.setImageBitmap(bitmap)
-                    val path = saveImage(bitmap)
-                    Toast.makeText(this@MainActivity, "QRCode saved to -> $path", Toast.LENGTH_SHORT).show()
+                    //val path = saveImage(bitmap)
+                    //Toast.makeText(this@MainActivity, "QRCode saved to -> $path", Toast.LENGTH_SHORT).show()
                 } catch (e: WriterException) {
                     e.printStackTrace()
                 }
             }
         }
     }
+
+
 
     @Throws(WriterException::class)
     private fun TextToImageEncode(Value: String): Bitmap? {
@@ -92,38 +127,7 @@ class MainActivity : AppCompatActivity() {
         return bitmap
     }
 
-    fun saveImage(myBitmap: Bitmap?): String {
-        val bytes = ByteArrayOutputStream()
-        myBitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val wallpaperDirectory = File(
-            Environment.getExternalStorageDirectory().toString() + "/QRcodeDemonuts")
-        // have the object build the directory structure, if needed.
 
-        if (!wallpaperDirectory.exists()) {
-            Log.d("dirrrrrr", "" + wallpaperDirectory.mkdirs())
-            wallpaperDirectory.mkdirs()
-        }
-
-        try {
-            val f = File(wallpaperDirectory, Calendar.getInstance()
-                .timeInMillis.toString() + ".jpg")
-            f.createNewFile()   //give read write permission
-            val fo = FileOutputStream(f)
-            fo.write(bytes.toByteArray())
-            MediaScannerConnection.scanFile(this,
-                arrayOf(f.path),
-                arrayOf("image/jpeg"), null)
-            fo.close()
-            Log.d("TAG", "File Saved::--->" + f.absolutePath)
-
-            return f.absolutePath
-        } catch (e1: IOException) {
-            e1.printStackTrace()
-        }
-
-        return ""
-
-    }
 
 
 
@@ -142,4 +146,6 @@ class MainActivity : AppCompatActivity() {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
+
+
 }
